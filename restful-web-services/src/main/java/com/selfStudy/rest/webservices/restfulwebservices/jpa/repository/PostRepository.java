00b9169e4c7ade9ee5fce49f535c7d7a3fa492d1
2194063 <jeromegarcia.16@gmail.com>
2023-10-12 03:35:03 +0800

    Initial for WebProj

--- restful-web-services/src/main/java/com/selfStudy/rest/webservices/restfulwebservices/jpa/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.selfStudy.rest.webservices.restfulwebservices.jpa.repository;
-
-import com.selfStudy.rest.webservices.restfulwebservices.jpa.post.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- restful-web-services/src/main/java/com/selfStudy/rest/webservices/restfulwebservices/jpa/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.selfStudy.rest.webservices.restfulwebservices.jpa.repository;
-
-import com.selfStudy.rest.webservices.restfulwebservices.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
